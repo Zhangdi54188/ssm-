822c6d57af9e5f8bce3ddf699c743f077f665286
Zhangdi <2140641497@qq.com>
2023-06-27 22:51:04 +0800

    feat:测试回退11

--- src/main/java/com/controller/GittTest.java
+++ src/main/java/com/controller/GittTest.java
@@ -2,7 +2,6 @@ package com.controller;
 
 public class GittTest {
     public static void main(String[] args) {
-        System.out.println("11111");
-        System.out.println("测试回退");
+
     }
 }
