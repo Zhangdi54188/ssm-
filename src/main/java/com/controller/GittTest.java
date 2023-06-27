02b7e2a20a3cfccb53f2551f3349e11dc54817a1
Zhangdi <2140641497@qq.com>
2023-06-27 23:31:11 +0800

    feat:zhangdi

--- src/main/java/com/controller/GittTest.java
+++ src/main/java/com/controller/GittTest.java
@@ -3,6 +3,8 @@ package com.controller;
 public class GittTest {
     public static void main(String[] args) {
         System.out.println("测试回退");
+        System.out.println("张迪");
+        System.out.println("你好");
 
     }
 }
